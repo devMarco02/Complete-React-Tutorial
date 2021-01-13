4783de5204fda012577214c7aaa682bb107e86b3
devMarco02 <75427627+devMarco02@users.noreply.github.com>
2021-01-13 23:45:14 +0800

    Update Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,20 +1,25 @@
 const Home = () => {
 
-  const handleClick = (e) => {
-    console.log('hello ninjas', e);
-  }
+	const clickHandler = () =>{   	
+		console.log("Hello nijas", );
+	}
 
-  const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target);
-  }
+	const clickHandlerAgain = (name) =>{
+		console.log(name);
+	}
 
-  return (
-    <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
-    </div>
-  );
+	const clickHandlerAgainAgain = (name, e) =>{  /* e(event parameter) */
+		console.log(name, e.target);
+	}
+
+	return (
+		<div className="home">
+			<h2>Homepage</h2>
+			<button onClick={clickHandler}>Click me</button> {/*automatically being invoked if adding () to the method, dont add ()*/}
+			<button onClick={ () => clickHandlerAgain('marco')}>Click me Again</button>  {/*if it has a parameter, wrap it inside an anonymous function*/}
+			<button onClick={ (e) => clickHandlerAgainAgain('mario', e)}>Click me Again</button>
+		</div>
+	  );
 }
  
 export default Home;
