c75889a9319b021e976013c625be173b2d198e29
devMarco02 <75427627+devMarco02@users.noreply.github.com>
2021-01-15 21:55:49 +0800

    Update BlogList.js

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,19 +1,21 @@
-const BlogList = ({ blogs, title}) => {
-  // const blogs = props.blogs;
-  // const title = props.title;
-  // console.log(blogs);
+// const BlogList = (props) => {
 
-  return (
-    <div className="blog-list">
-      <h2>{ title }</h2>
-      {blogs.map(blog => (
-        <div className="blog-preview" key={blog.id} >
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
-        </div>
-      ))}
-    </div>
-  );
+// 	const blogs = props.blogs;      //this works too
+// 	const title = props.title;
+
+const BlogList = ({blogs, title}) => {   //direct
+
+	return ( 
+		<div className="blog-list">
+				<h2> {title} </h2>
+				{blogs.map( (blog) => (
+					<div className="blog-preview" key={blog.id}>
+						<h2>{blog.title}</h2>
+						<p>Written by: {blog.author}</p>
+					</div>
+			))}
+		</div>
+	 );
 }
  
 export default BlogList;
