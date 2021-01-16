50834a09a7e2b6e26ae6bba2bef96983b99588ec
devMarco02 <75427627+devMarco02@users.noreply.github.com>
2021-01-16 22:55:51 +0800

    Update Home.js

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,31 +1,33 @@
-import { useEffect, useState } from "react";
-import BlogList from "./BlogList";
+import { useState, useEffect} from 'react';
+import BlogList from './Bloglist';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
 
-  const [name, setName] = useState('mario');
+	const [blogs, setBlogs] = useState([
+		{title: 'My new website', body: 'lorem ipsum ...', author: 'mario', id: '1'},
+		{title: 'Welcome party', body: 'lorem ipsum ...', author: 'yoshi', id: '2'},
+		{title: 'Web dev top tips', body: 'lorem ipsum ...', author: 'mario', id: '3'}
+	])
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+	const [name, setName] = useState('mario');
 
-  useEffect(() => {
-    console.log('use effect ran');
-    console.log(blogs);
-  }, [name])
+	const handleDelete = (id) => {
+		const newBlogs = blogs.filter((blog) => blog.id !== id);
+		setBlogs(newBlogs);
+	}
 
-  return (
-    <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('luigi')}>change name</button>
-    </div>
-  );
+	useEffect(() => {            //useEffect runs everytime the component is rendered
+		console.log("use effect ran");
+		console.log(name);
+	// }, []);                  []empty array to run useEffect once
+	}, [name]);                 //useEffect runs everytime the dependency(name) is changed
+
+	return (
+		<div className="home">
+			<BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+			<button onClick={() => setName('luigi')}>change name</button>
+		</div>
+	  );
 }
  
 export default Home;
